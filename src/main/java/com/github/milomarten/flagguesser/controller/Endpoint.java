b617756d8b4e7a4816a4855a9e09b7707a92c272
milomarten <milomartenstreams@gmail.com>
2025-02-17 22:40:55 -0600

    Remove /vexle from path, since it's redundant

--- src/main/java/com/github/milomarten/flagguesser/controller/Endpoint.java
+++ src/main/java/com/github/milomarten/flagguesser/controller/Endpoint.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/vexle/api")
+@RequestMapping("/api")
 public class Endpoint {
     @Autowired
     private FlagCompareService flagCompareService;
